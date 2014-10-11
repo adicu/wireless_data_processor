8046b99bb523f3736a4c9120c33af95f89ae0560
Nate Brennand <natebrennand@gmail.com>
2014-10-11 15:45:16 -0400

    basic setup for everything

--- data.go
+++ data.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"strconv"
+
+	"github.com/lib/pq"
 )
 
+// dumpFormat represents the datapoints provided for the wireless data.
 type dumpFormat struct {
 	ID          int
 	Name        string `json:"name"`
@@ -14,11 +18,12 @@ type dumpFormat struct {
 	ParentID    int    `json:"parent_id"`
 }
 
+// parseData unmarshals a byte array into an array of wireless data dumps.
 func parseData(datafile []byte) ([]dumpFormat, error) {
 	var parsed map[string]dumpFormat = make(map[string]dumpFormat)
 	err := json.Unmarshal(datafile, &parsed)
 	if err != nil {
-		return []dumpFormat{}, fmt.Errorf("Error parsing file => %s", err.Error())
+		return []dumpFormat{}, fmt.Errorf("Error parsing bytes => %s", err.Error())
 	}
 
 	var data []dumpFormat = make([]dumpFormat, len(parsed))
@@ -36,4 +41,38 @@ func parseData(datafile []byte) ([]dumpFormat, error) {
 	}
 
 	return data, nil
+}
+
+// dataset is an alias for an array of data dumps
+type dataset []dumpFormat
+
+// insert operates on a list of dumpFormat and inserts them to the provided Postgres
+// database.
+func (data dataset) insert(db *sql.DB) error {
+	txn, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("Error starting PG txn => %s", err.Error())
+	}
+
+	stmt, err := txn.Prepare(pq.CopyIn("name", "count"))
+	if err != nil {
+		return fmt.Errorf("Error prepping PG txn => %s", err.Error())
+	}
+	defer stmt.Close()
+
+	for _, d := range data {
+		_, err = stmt.Exec(d.Name, d.ClientCount)
+		if err != nil {
+			return fmt.Errorf("Failed to add to bulk insert => %s", err.Error())
+		}
+	}
+
+	if _, err = stmt.Exec(); err != nil {
+		return fmt.Errorf("Failed to execute bulk insert => %s", err.Error())
+	}
+
+	if err = txn.Commit(); err != nil {
+		log.Fatal("Failed to commit txn => %s", err.Error())
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,110 @@
 package main
 
-import ()
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/howeyc/fsnotify"
+	_ "github.com/lib/pq"
+)
+
+// getOrElse checks the specified environment variable, returns the value if found, otherwise
+// will return the default value provided. If there is no default then makes a fatal log.
+func getOrElse(key, standard string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	} else if standard == "" {
+		log.Fatalf("The environment variable, {}, must be set", key)
+	}
+	return standard
+}
+
+// dbConnect yanks db configurations from the environment variables and returns a postgres
+// connection
+func dbConnect() *sql.DB {
+	db, err := sql.Open("postgres",
+		fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s",
+			getOrElse("PG_USER", "adicu"),
+			getOrElse("PG_DB", ""),
+			getOrElse("PG_PASSWORD", ""),
+			getOrElse("PG_HOST", "localhost"),
+			getOrElse("PG_PORT", "5432"),
+		))
+	if err != nil {
+		log.Fatalf("Error connecting to Postgres => %s", err.Error())
+	}
+	return db
+}
+
+// handleFile processes new files
+//
+// The file is read into memory, parsed then inserted to the database.
+func handleFile(filename string, db *sql.DB) {
+	log.Printf("Processing, %s", filename)
+	fileContents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Failed to read in file => %s", err.Error())
+	}
+
+	data, err := parseData(fileContents)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = dataset(data).insert(db); err != nil {
+		log.Printf("Failed to insert data from, %s =>", filename, err.Error())
+	}
+}
 
 func main() {
+	db := dbConnect()
+	watchDir := *flag.String("dir", ".", "directory to watch for new files")
 
+	if *flag.Bool("all", false, "load all dump file in the directory") {
+		log.Printf("Loading all files in directory, %s", watchDir)
+
+		files, err := ioutil.ReadDir(watchDir)
+		if err != nil {
+			log.Fatalf("Failed to read in directory info => %s", err.Error())
+		}
+
+		for _, f := range files {
+			handleFile(f.Name(), db)
+		}
+	}
+
+	if !*flag.Bool("watch", true, "continue to watch for new files in the directory") {
+		log.Println("Not watching for files as specified")
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal("Failed to instantiate file watcher")
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Event:
+				log.Println(event)
+				if event.IsCreate() {
+					handleFile(event.Name, db)
+				}
+			case err := <-watcher.Error:
+				log.Println(err)
+			}
+		}
+	}()
+
+	if err = watcher.Watch(watchDir); err != nil {
+		log.Fatalf("Failed to start watching directory, %s => %s", watchDir, err.Error())
+	}
+
+	<-done
 }
