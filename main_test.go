5be504260c17310164e18e72142b0a26b965a2b2
Nate Brennand <natebrennand@gmail.com>
2014-10-12 18:23:14 -0400

    add dates stuff

--- main_test.go
+++ main_test.go
@@ -1,9 +1,24 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+var (
+	testFilename = "2014-10-11-15-45.json"
+	expectedTime = time.Date(2014, time.October, 11, 15, 45, 0, 0, &time.Location{})
+)
 
 func TestFilenameRegex(t *testing.T) {
-	if !filenameRegex.MatchString("2014-10-11-15-45.json") {
+	if !filenameRegex.MatchString(testFilename) {
 		t.Error("regex did not properly match")
+	}
+}
+
+func TestPullDate(t *testing.T) {
+	tm, err := getDate(testFilename)
+	if err != nil || tm.String() != expectedTime.String() {
+		t.Errorf("Failed to properly parse date, found %s, expected %s", tm, expectedTime)
 	}
 }
