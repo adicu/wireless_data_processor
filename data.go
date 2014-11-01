3107fff053a213dab0b39fe2e0560a35f7bebb09
Nate Brennand <natebrennand@gmail.com>
2014-11-01 09:19:02 -0400

    update for declared schema

--- data.go
+++ data.go
@@ -11,17 +11,32 @@ import (
 	"github.com/lib/pq"
 )
 
-// dumpFormat represents the datapoints provided for the wireless data.
-type dumpFormat struct {
-	ID          int
-	DumpTime    time.Time
-	Name        string `json:"name"`
-	ClientCount int    `json:"client_count"`
-	ParentID    int    `json:"parent_id"`
+var parentNameLookup = map[int]string{
+	146: "Avery",
+	103: "Butler",
+	62:  "East Asian Library",
+	75:  "John Jay",
+	79:  "Lehman LIbrary",
+	84:  "Lerner",
+	15:  "Mudd",
+	2:   "Uris",
 }
 
+// dumpFormat represents the datapoints provided for the wireless data.
+type dumpFormat struct {
+	DumpTime    time.Time
+	GroupID     int
+	GroupName   string `json:"name"`
+	ParentID    int    `json:"parent_id"`
+	ParentName  string
+	ClientCount int `json:"client_count"`
+}
+
+// dataset is an alias for an array of data dumps
+type dataset []dumpFormat
+
 // parseData unmarshals a byte array into an array of wireless data dumps.
-func parseData(timestamp time.Time, datafile []byte) ([]dumpFormat, error) {
+func parseData(timestamp time.Time, datafile []byte) (dataset, error) {
 	var parsed map[string]dumpFormat = make(map[string]dumpFormat)
 	err := json.Unmarshal(datafile, &parsed)
 	if err != nil {
@@ -37,17 +52,20 @@ func parseData(timestamp time.Time, datafile []byte) ([]dumpFormat, error) {
 			continue
 		}
 
-		d.ID = int(parsedInt)
+		d.GroupID = int(parsedInt)
 		d.DumpTime = timestamp
+
+		var exists bool
+		d.ParentName, exists = parentNameLookup[d.ParentID]
+		if !exists {
+			log.Printf("WARN: no parent name for %d exists", d.ParentID)
+		}
 		data[i] = d
 		i++
 	}
 
 	return data, nil
 }
-
-// dataset is an alias for an array of data dumps
-type dataset []dumpFormat
 
 // insert operates on a list of dumpFormat and inserts them to the provided Postgres
 // database.
@@ -57,14 +75,28 @@ func (data dataset) insert(db *sql.DB) error {
 		return fmt.Errorf("Error starting PG txn => %s", err.Error())
 	}
 
-	stmt, err := txn.Prepare(pq.CopyIn("name", "count"))
+	stmt, err := txn.Prepare(pq.CopyIn(
+		"dump_time",
+		"group_id",
+		"group_name",
+		"parent_id",
+		"parent_name",
+		"client_count",
+	))
 	if err != nil {
 		return fmt.Errorf("Error prepping PG txn => %s", err.Error())
 	}
 	defer stmt.Close()
 
 	for _, d := range data {
-		_, err = stmt.Exec(d.Name, d.ClientCount)
+		_, err = stmt.Exec(
+			d.DumpTime,
+			d.GroupID,
+			d.GroupName,
+			d.ParentID,
+			d.ParentName,
+			d.ClientCount,
+		)
 		if err != nil {
 			return fmt.Errorf("Failed to add to bulk insert => %s", err.Error())
 		}
