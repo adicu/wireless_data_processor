fa68eabc263383bc9a2d888ac7b0c7de6e495316
Nate Brennand <natebrennand@gmail.com>
2015-04-18 15:26:22 -0400

    reorganize things and update group names
    closes #4

--- main.go
+++ main.go
@@ -16,20 +16,32 @@ import (
 )
 
 var (
+	// example: 2014-10-31-15-15.json
 	filenameRegex     = regexp.MustCompile(`(\d{4}(-\d{2}){4})\.json$`)
 	datetimeRegex     = regexp.MustCompile(`([\d-]*)`)
 	datetimeFormat    = "2006-01-02-15-04"
+	NY                *time.Location
 	materializedViews = []string{
 		"hour_window",
 		"day_window",
 		"week_window",
 		"month_window",
 	}
-	maxHandlesNum                                         = 100
 	PG_USER, PG_PASSWORD, PG_DB, PG_HOST, PG_PORT, PG_SSL string
 )
 
-func refreshDBCredentials() {
+// init is called on startup
+func init() {
+	var err error
+	NY, err = time.LoadLocation("America/New_York")
+	if err != nil {
+		log.Fatalf("Cannot load NYC tz => {%s}", err)
+	}
+
+}
+
+// configure runs before startup
+func configure() {
 	PG_USER = getOrElse("PG_USER", "adicu")
 	PG_PASSWORD = getOrElse("PG_PASSWORD", "")
 	PG_DB = getOrElse("PG_DB", "")
@@ -41,11 +53,10 @@ func refreshDBCredentials() {
 // getDate parses a filepath to get a date from the filename given the regex
 // declared in `filenameRegex`.
 func getDate(s string) (time.Time, error) {
-	tz, err := time.LoadLocation("America/New_York")
-	if err != nil {
-		return time.Time{}, err
-	}
-	return time.ParseInLocation(datetimeFormat, datetimeRegex.FindString(path.Base(s)), tz)
+	return time.ParseInLocation(
+		datetimeFormat,
+		datetimeRegex.FindString(path.Base(s)),
+		NY)
 }
 
 // getOrElse checks the specified environment variable, returns the value if found, otherwise
@@ -62,10 +73,6 @@ func getOrElse(key, standard string) string {
 // dbConnect yanks db configurations from the environment variables and returns a postgres
 // connection
 func dbConnect() *sql.DB {
-	if PG_USER == "" {
-		refreshDBCredentials()
-	}
-
 	db, err := sql.Open("postgres",
 		fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=%s",
 			PG_USER,
@@ -129,7 +136,63 @@ func updateViews(db *sql.DB) {
 	}
 }
 
+func LoadAllFiles(watchDir string) {
+	log.Printf("Loading all files in directory, %s", watchDir)
+
+	db := dbConnect()
+	defer db.Close()
+
+	files, err := ioutil.ReadDir(watchDir)
+	if err != nil {
+		log.Fatalf("Failed to read in directory info => %s", err.Error())
+	}
+
+	// handle every data file
+	for _, f := range files {
+		handleFile(path.Join(watchDir, f.Name()), db)
+	}
+
+	updateViews(db) // refresh the materialized views afterwards
+}
+
+func watchDirectory(watchDir string) {
+	// start watching for new files
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal("Failed to instantiate file watcher")
+	}
+	defer watcher.Close()
+
+	// start the file system watcher
+	if err = watcher.WatchFlags(watchDir, fsnotify.FSN_CREATE); err != nil {
+		log.Fatalf("Failed to start watching directory, %s => %s", watchDir, err.Error())
+	}
+
+	// wait for any new files to be added, then process them
+	for {
+		select {
+		case event := <-watcher.Event:
+			// reconnect to the DB for each event because otherwise the connection gets stale
+			db := dbConnect()
+			if filenameRegex.MatchString(event.Name) {
+				// sleep to allow the whole file to be transmitted.
+				// otherwise we get a parsing error because it's incomplete.
+				time.Sleep(time.Duration(2 * time.Second))
+
+				handleFile(event.Name, db)
+				updateViews(db)
+			}
+			db.Close()
+		case err := <-watcher.Error:
+			log.Println(err)
+		}
+	}
+}
+
 func main() {
+	configure() // set up all configuration variables
+
+	// gather CLI configurations
 	var (
 		watchDir     = flag.String("dir", ".", "directory to watch for new files")
 		loadAll      = flag.Bool("all", false, "load all dump file in the directory")
@@ -139,54 +202,14 @@ func main() {
 
 	// if all the files currently in the directory should be loaded
 	if *loadAll {
-		db := dbConnect()
-
-		log.Printf("Loading all files in directory, %s", *watchDir)
-		files, err := ioutil.ReadDir(*watchDir)
-		if err != nil {
-			log.Fatalf("Failed to read in directory info => %s", err.Error())
-		}
-
-		// handle every data file
-		for _, f := range files {
-			handleFile(path.Join(*watchDir, f.Name()), db)
-		}
-
-		updateViews(db) // refresh the materialized views afterwards
-		db.Close()
+		LoadAllFiles(*watchDir)
 	}
 
 	// exits if flag turned on
-	if !*keepWatching {
-		log.Println("Not watching for files as specified")
-		return
+	if *keepWatching {
+		watchDirectory(*watchDir)
 	}
 
-	// start watching for new files
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal("Failed to instantiate file watcher")
-	}
-	defer watcher.Close()
-
-	// start the file system watcher
-	if err = watcher.WatchFlags(*watchDir, fsnotify.FSN_CREATE); err != nil {
-		log.Fatalf("Failed to start watching directory, %s => %s", *watchDir, err.Error())
-	}
-
-	// wait for any new files to be added, then process them
-	for {
-		select {
-		case event := <-watcher.Event:
-			db := dbConnect()
-			if filenameRegex.MatchString(event.Name) {
-				time.Sleep(time.Duration(2 * time.Second))
-				handleFile(event.Name, db)
-				updateViews(db)
-			}
-			db.Close()
-		case err := <-watcher.Error:
-			log.Println(err)
-		}
-	}
+	// log because it's an unexpected answer
+	log.Println("Not watching for files as specified")
 }
